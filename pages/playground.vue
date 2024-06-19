be8bfa20209e529e36564bdcc9b9cb1f9a83a889
Jeremy THEBAULT <j.thebault@techfirm.ch>
2024-06-19 15:23:12 +0200

    Fetch server site

---
+++ pages/playground.vue
@@ -0,0 +1,10 @@
+<template>
+<!--  <div class="inline-block p-6 bg-gradient-to-bl from-green-400 to-blue-400"></div>-->
+  <div class="mt-10 ml-10 w-[1280px] h-[769px] bg-gradient-to-bl from-[#FFFBE3] to-[#FFD031]">
+
+  </div>
+</template>
+
+<script setup>
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
+</script>
---
+++ pages/products.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="grid grid-cols-5 gap-4">
+    <div
+        v-for="product in products"
+        class="flex flex-col shadow-md bg-white p-6 rounded-md"
+    >
+      <img class="w-[75px] h-auto self-center" :src="product.image" alt="" />
+      <h2 class="text-black mt-auto text-sm">{{ product.title }}</h2>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
+</script>
+<style lang="scss" scoped>
+
+</style>
